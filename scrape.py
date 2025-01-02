ad880369328a8da4d08fd6504a8db32dc24a4efc
Kevin Ayers <home@Kevins-MacBook-Air.local>
2025-01-01 17:56:14 -0800

    initial setup

---
+++ scrape.py
@@ -0,0 +1,28 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+
+def scrape_website(website_url):
+    print("Launching Chrome Browser...")
+
+
+    chrome_driver_path = ""
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+
+    try:
+        driver.get(website_url)
+        print("Page loaded successfully...")
+        html = driver.page_source
+
+        time.sleep(10)
+
+        return html
+
+    finally:
+        driver.quit()
+
+
+
